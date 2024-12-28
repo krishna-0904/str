00263e0b738bc69e1bbe186d591be2f0ef0416d3
krishna-0904 <krishnaprabhas601@gmail.com>
2024-12-28 08:27:19 +0530

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1,7 +1,7 @@
 corr_ans=0
 incorr_ans=0
 print("1.what is the capital of india?")
-user_ans=("enter answer:").lower()
+user_ans=input("enter answer:").lower()
 print(user_ans)
 if user_ans=="delhi":
     corr_ans+=1
@@ -9,4 +9,4 @@ if user_ans=="delhi":
 else:
     incorr_ans+=1
     print("incorrect answer")
-print("percentage",(corr_ans/(corr_ans+incorr_ans))*100)
+print("percentage",((corr_ans/(corr_ans+incorr_ans))*100))
