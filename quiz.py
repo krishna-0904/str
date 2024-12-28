be4ece784e917edeba28bf496b615a292f6155b4
krishna-0904 <krishnaprabhas601@gmail.com>
2024-12-28 07:13:37 +0530

    Create quiz.py

---
+++ quiz.py
@@ -0,0 +1,12 @@
+corr_ans=0
+incorr_ans=0
+print("1.what is the capital of india?")
+user_ans=("enter answer:").lower()
+print(user_ans)
+if user_ans=="delhi":
+    corr_ans+=1
+    print("correct answer")
+else:
+    incorr_ans+=1
+    print("incorrect answer")
+print("percentage",(corr_ans/(corr_ans+incorr_ans))*100)
