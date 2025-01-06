7b0c8e1b24e50f7abd9463651d7c934ed3aa1af7
krishna-0904 <krishnaprabhas601@gmail.com>
2025-01-06 17:19:56 +0530

    Update str.py

--- str.py
+++ str.py
@@ -1,1 +1,13 @@
-
+q=['1.what is the capital of india?',
+    '2.what is the national animal?',
+    '3.who is the hero of bahubali?',
+    '4.who is rajamouli?',
+    '5.where is banglore?']
+ans=['delhi','tiger', 'prabhas', 'directot','karnataka']
+for x in range(len(q)):
+    print(q[x])
+    user_ans=input('enter answer:').lower()
+    if user_ans==ans[x]:
+        print('correct ans')
+    else:
+        print('wrong ans')
